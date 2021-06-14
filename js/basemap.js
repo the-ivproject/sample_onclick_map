b66ee0cad2526442f7677ecd2fdcdb36eb2f1e8f
the-ivproject <84888011+the-ivproject@users.noreply.github.com>
2021-06-14 23:24:48 +0700

    Add files via upload

---
+++ js/basemap.js
@@ -0,0 +1,48 @@
+const map = L.map('map', null, { zoomControl:true }).setView([43.329, -88.161]);
+let layer = L.esri.basemapLayer('Topographic').addTo(map);
+let layerLabels;
+
+function setBasemap(basemap) {
+    if (layer) {
+        map.removeLayer(layer);
+    }
+
+    layer = L.esri.basemapLayer(basemap);
+
+    map.addLayer(layer);
+
+    if (layerLabels) {
+        map.removeLayer(layerLabels);
+    }
+
+    if (
+        basemap === 'ShadedRelief' ||
+        basemap === 'Oceans' ||
+        basemap === 'Gray' ||
+        basemap === 'DarkGray' ||
+        basemap === 'Terrain'
+    ) {
+        layerLabels = L.esri.basemapLayer(`${basemap}Labels`);
+        map.addLayer(layerLabels);
+    } else if (basemap.includes('Imagery')) {
+        layerLabels = L.esri.basemapLayer('ImageryLabels');
+        map.addLayer(layerLabels);
+    }
+}
+
+let basemaps = document.getElementById('inlineFormCustomSelect');
+
+basemaps.addEventListener('change', function () {
+    setBasemap(basemaps.value);
+});
+
+    
+function ZoomIn() {
+    map.zoomIn();
+}
+
+function ZoomOut() {
+    map.zoomOut();
+}
+
+
---
+++ js/index.js
@@ -0,0 +1,122 @@
+const country = 'assets/countryBorders.geo.json'
+const point = 'assets/sumOlympics.geo.json'
+
+let polyStyle = {
+    fillColor: '#37991e',
+    fillOpacity: 0.2,
+    weight: 2,
+    color: '#023047',
+    opacity: 1
+}
+
+let polyStyleOnclick = {
+    weight: 5,
+    color: '#1d3557',
+    dashArray: '',
+    fillOpacity: 0.7
+}
+
+let geojsonMarkerOptions = {
+    radius: 5,
+    fillColor: "#e63946",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 1
+};
+
+let iconImage = {
+    iconUrl: 'assets/icon/pin.png',
+    iconSize: [25, 25], // width and height of the image in pixels
+    shadowSize: [35, 20], // width, height of optional shadow image
+    iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
+    shadowAnchor: [12, 6], // anchor point of the shadow. should be offset
+    popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
+}
+
+let countryGeo = $.ajax({
+    url: country,
+    dataType: "json",
+    success: console.log("Country data successfully loaded."),
+    error: function (xhr) {
+        alert(xhr.statusText)
+    }
+})
+
+let pointGeo = $.ajax({
+    url: point,
+    dataType: "json",
+    success: console.log("Point data successfully loaded."),
+    error: function (xhr) {
+        alert(xhr.statusText)
+    }
+})
+
+$.when(countryGeo, pointGeo).done(() => {
+
+    let countrylayer = countryGeo.responseJSON
+    let pointlayer = pointGeo.responseJSON
+
+    let lastClickedLayer = null
+
+    let OnClickFeature = e => {
+        if (lastClickedLayer !== null) {
+            lastClickedLayer.setStyle(polyStyle);
+        }
+
+        map.fitBounds(e.target.getBounds());
+        let countrylayer = e.target;
+
+        lastClickedLayer = countrylayer;
+
+        countrylayer.setStyle(polyStyleOnclick);
+
+        let popup = L.popup()
+            .setContent(`Country: ${countrylayer.feature.properties.name}`);
+
+        countrylayer.bindPopup(popup).openPopup();
+    }
+
+    function onEachFeature(feature, layer) {
+        layer.on({
+            click: OnClickFeature
+        });
+    }
+
+    let circleStyle = (feature, latlng) => {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+
+    let IconImage = (feature, latlng) => {
+        let myIcon = L.icon(iconImage)
+        return L.marker(latlng, {
+            icon: myIcon
+        })
+    }
+
+    let geojson = L.geoJSON(countrylayer, {
+        onEachFeature: onEachFeature,
+        style: polyStyle
+    })
+
+    let geojsonPoint = L.geoJSON(pointlayer, {
+        pointToLayer: IconImage
+    })
+
+    map.addLayer(geojson)
+    map.addLayer(geojsonPoint)
+
+    map.fitBounds(geojson.getBounds())
+
+    document.getElementById('fitbound').onclick = function () {
+        map.fitBounds(geojson.getBounds())
+        lastClickedLayer.setStyle(polyStyle)
+    }
+    document.getElementById('removelayer').onclick = function () {
+        if (!this.checked) {
+            map.removeLayer(geojsonPoint)
+        } else {
+            map.addLayer(geojsonPoint)
+        }
+    }
+})
